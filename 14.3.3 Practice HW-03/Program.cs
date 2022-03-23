869b5a7d30f1c5657bc1852d1c4ff89919ffda0d
vdv7105131 <vdv7105131@gmail.com>
2022-03-23 11:47:24 +0300

    Добавьте файлы проекта.

---
+++ 14.3.3 Practice HW-03/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace _14._3._3_Practice_HW_03
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
