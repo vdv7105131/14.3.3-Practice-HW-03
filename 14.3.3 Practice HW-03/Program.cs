4e6005f69e88e9789bf2b9d50e60e64f7b9c1c74
vdv7105131 <vdv7105131@gmail.com>
2022-03-23 11:49:36 +0300

    ok

--- 14.3.3 Practice HW-03/Program.cs
+++ 14.3.3 Practice HW-03/Program.cs
@@ -1,12 +1,47 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 
-namespace _14._3._3_Practice_HW_03
+// Доработайте вашу телефонную книгу из задания 14.2.10 предыдущего юнита так,
+// чтобы контакты телефонной книги были отсортированы сперва по имени, а затем по фамилии.
+
+namespace _14._3._3_Практика__HW_03_
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            //  создаём пустой список с типом данных Contact
+            var phoneBook = new List<Contact>();
+
+            // добавляем контакты
+            phoneBook.Add(new Contact("Игорь", "Николаев", 79990000001, "igor@example.com"));
+            phoneBook.Add(new Contact("Сергей", "Довлатов", 79990000010, "serge@example.com"));
+            phoneBook.Add(new Contact("Анатолий", "Карпов", 79990000011, "anatoly@example.com"));
+            phoneBook.Add(new Contact("Валерий", "Леонтьев", 79990000012, "valera@example.com"));
+            phoneBook.Add(new Contact("Сергей", "Брин", 799900000013, "serg@example.com"));
+            phoneBook.Add(new Contact("Иннокентий", "Смоктуновский", 799900000013, "innokentii@example.com"));
+
+            var sortedStuds = from s in phoneBook orderby s.Name, s.LastName select s;
+
+            foreach (var i in sortedStuds)
+                Console.WriteLine(i.Name + " " + i.LastName);
         }
+    }
+
+    public class Contact // модель класса
+    {
+        public Contact(string name, string lastName, long phoneNumber, String email) // метод-конструктор
+        {
+            Name = name;
+            LastName = lastName;
+            PhoneNumber = phoneNumber;
+            Email = email;
+        }
+
+        public String Name { get; }
+        public String LastName { get; }
+        public long PhoneNumber { get; }
+        public String Email { get; }
     }
 }
